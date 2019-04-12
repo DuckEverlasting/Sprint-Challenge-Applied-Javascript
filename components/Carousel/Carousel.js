6d15e48ca5446bf960b3db294cd1df84709ddbf6
Matt Klein <mklein246@gmail.com>
2019-04-12 11:47:48 -0700

    less is being weird, but I managed to get the carousel working!

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,39 @@
 class Carousel {
-    
+    constructor(el) {
+        this.el = el;
+        this.leftButton = el.querySelector(".left-button");
+        this.rightButton = el.querySelector(".right-button");
+        this.images = el.querySelectorAll(".carousel-img");
+        this.images[0].classList.add("visible") // STARTING IMAGE
+        this.currentIndex = 0; // STARTING INDEX
+        this.currentImage = el.querySelector(`img[data-index="${this.currentIndex}"]`);
+        this.leftButton.addEventListener("click", () => {this.moveLeft()});
+        this.rightButton.addEventListener("click", () => {this.moveRight()});
+    }
+    moveLeft() {
+        if(this.currentIndex === 0) {
+            this.currentIndex = (this.images.length - 1);
+        } else {
+            this.currentIndex --;
+        }
+        this.images.forEach(el => el.classList.remove("visible"));
+        this.images[this.currentIndex].classList.add("visible");
+        console.log(this.currentIndex);
+    }
+
+    moveRight() {
+        if(this.currentIndex === (this.images.length - 1)) {
+            this.currentIndex = 0;
+        } else {
+            this.currentIndex ++;
+        }
+        this.images.forEach(el => el.classList.remove("visible"));
+        this.images[this.currentIndex].classList.add("visible");
+    }
 }
 
 let carousel = document.querySelector(".carousel");
+new Carousel(carousel);
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
